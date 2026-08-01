c565a92d521b4af0042039b9390f2d4a2c33e64f
hopcourses <cocoriconcept@gmail.com>
2026-08-01 21:52:51 +0200

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,40 +1,53 @@
-// =====================================
-// Carte
-// =====================================
-
 const map = L.map('map',{
-
-zoomControl:false
-
+    zoomControl:false
 });
 
 L.tileLayer(
-
 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-
 {
+    attribution:'© OpenStreetMap'
+}).addTo(map);
 
-attribution:'© OpenStreetMap'
+let markerCollecte;
+let markerLivraison;
+let ligne;
+
+function afficherItineraire(course){
+
+    if(markerCollecte) map.removeLayer(markerCollecte);
+    if(markerLivraison) map.removeLayer(markerLivraison);
+    if(ligne) map.removeLayer(ligne);
+
+    markerCollecte = L.marker(course.collecte).addTo(map);
+
+    markerLivraison = L.marker(course.livraison).addTo(map);
+
+    ligne = L.polyline(
+
+        [
+
+            course.collecte,
+
+            course.livraison
+
+        ],
+
+        {
+
+            color:"#F28C18",
+
+            weight:5
+
+        }
+
+    ).addTo(map);
+
+    map.fitBounds(ligne.getBounds(),{
+
+        padding:[60,60]
+
+    });
 
 }
 
-).addTo(map);
-
-map.setView(courses[0].collecte,11);
-
-
-// Création des marqueurs
-
-courses.forEach(course=>{
-
-const marker=L.marker(course.collecte).addTo(map);
-
-marker.on("click",()=>{
-
-afficherCourse(course);
-
-map.panTo(course.collecte);
-
-});
-
-});
+afficherItineraire(courses[0]);
