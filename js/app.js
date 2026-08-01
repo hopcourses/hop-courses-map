b19a2b50475ebddec5b9e0da80b2eabc984ff6f8
hopcourses <cocoriconcept@gmail.com>
2026-08-01 21:52:35 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,18 +1,3 @@
-// =====================================
-// Démarrage
-// =====================================
+indexCourse = 0;
 
-// Si aucune course
-if(courses.length===0){
-
-    afficherAucuneCourse();
-
-}
-
-// Sinon
-
-else{
-
-    afficherCourse(courses[0]);
-
-}
+afficherCourse(indexCourse);
