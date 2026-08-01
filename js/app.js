5e361f28422cb69b384c5ecd5b4fe401c7ab6c50
hopcourses <cocoriconcept@gmail.com>
2026-08-01 21:47:29 +0200

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// =====================================
+// Démarrage
+// =====================================
+
+// Si aucune course
+if(courses.length===0){
+
+    afficherAucuneCourse();
+
+}
+
+// Sinon
+
+else{
+
+    afficherCourse(courses[0]);
+
+}
