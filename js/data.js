70f273a611effd306975b48f2c33b788d6c34ec5
hopcourses <cocoriconcept@gmail.com>
2026-08-02 01:54:47 +0200

    Test chargement JSON

--- js/data.js
+++ js/data.js
@@ -1,36 +1,22 @@
-const courses = [
+alert("data.js chargé");
 
-{
-    id:1,
-    depart:"Boeschepe",
-    arrivee:"Bailleul",
-    gain:"8,50 €",
-    heure:"19h30 - 20h00",
-    type:"Drive alimentaire • Taille S",
-    collecte:[50.8018,2.6905],
-    livraison:[50.7358,2.7358]
-},
+let courses = [];
 
-{
-    id:2,
-    depart:"Hazebrouck",
-    arrivee:"Steenvoorde",
-    gain:"6,00 €",
-    heure:"09h00 - 10h00",
-    type:"Drive alimentaire • Taille S",
-    collecte:[50.7236,2.5370],
-    livraison:[50.8100,2.5830]
-},
+async function chargerCourses() {
+    try {
+        const response = await fetch("../data.json");
 
-{
-    id:3,
-    depart:"Dunkerque",
-    arrivee:"Bergues",
-    gain:"9,20 €",
-    heure:"18h00 - 18h30",
-    type:"Drive alimentaire • Taille M",
-    collecte:[51.0344,2.3768],
-    livraison:[50.9680,2.4320]
+        if (!response.ok) {
+            throw new Error("Impossible de charger data.json");
+        }
+
+        courses = await response.json();
+
+        console.log("Courses chargées :", courses);
+
+    } catch (error) {
+        console.error(error);
+    }
 }
 
-];
+chargerCourses();
