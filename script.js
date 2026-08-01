dd558b927fa58c3fde05a8a5b8e8d8818ac44616
hopcourses <cocoriconcept@gmail.com>
2026-08-01 18:36:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+// Création de la carte
+const map = L.map('map', {
+    zoomControl: false
+}).setView([46.603354, 1.888334], 6);
 
+// Fond de carte OpenStreetMap
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; OpenStreetMap'
+}).addTo(map);
+
+// Exemple de position du cotransporteur (à remplacer plus tard)
+const courier = L.marker([48.8566, 2.3522]).addTo(map);
+
+courier.bindPopup("🚚 Vous êtes ici");
