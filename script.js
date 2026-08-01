81b4d1f4027d4b083c2d529c39efa0d0d222bad2
hopcourses <cocoriconcept@gmail.com>
2026-08-01 19:03:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const map = L.map('map').setView([48.8566, 2.3522], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; OpenStreetMap'
+    attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-L.marker([48.8566, 2.3522]).addTo(map)
-    .bindPopup("Paris")
+L.marker([48.8566, 2.3522])
+    .addTo(map)
+    .bindPopup('Bonjour Hop Courses !')
     .openPopup();
